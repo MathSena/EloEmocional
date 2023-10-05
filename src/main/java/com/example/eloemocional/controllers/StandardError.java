a75e5c58e5f4a915fde4078e5306fa90a454ba5d
MathSena <MathSena07@hotmail.com>
2023-10-05 16:50:43 +0100

    Create new classes: FieldMessage.java, ResourceExceptionHandler.java, StandardError.java, and ValidationError.java.

---
+++ src/main/java/com/example/eloemocional/controllers/StandardError.java
@@ -0,0 +1,21 @@
+package com.example.eloemocional.controllers;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class StandardError implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Long timestamp;
+    private Integer status;
+    private String error;
+    private String message;
+    private String path;
+
+}
---
+++ src/main/java/com/example/eloemocional/controllers/ValidationError.java
@@ -0,0 +1,31 @@
+package com.example.eloemocional.controllers;
+
+import lombok.AllArgsConstructor;
+
+import java.io.Serial;
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+public class ValidationError extends StandardError {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private List<FieldMessage> errors = new ArrayList<>();
+
+    public ValidationError() {
+
+    }
+
+    public ValidationError(Long timestamp, Integer status, String error, String message, String path) {
+        super(timestamp, status, error, message, path);
+    }
+
+    public List<FieldMessage> getErrors() {
+        return errors;
+    }
+
+    public void addError(String fielName, String message) {
+        this.errors.add(new FieldMessage(fielName, message));
+    }
+}
